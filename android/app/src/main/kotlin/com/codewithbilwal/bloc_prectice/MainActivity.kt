94c57ac80041d5bf4f1651ad3a722609a7f60623
bilwal <bilawal.mehmood@sapphire.co>
2024-11-06 12:38:30 +0500

    Counter added

---
+++ android/app/src/main/kotlin/com/codewithbilwal/bloc_prectice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codewithbilwal.bloc_prectice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bloc_prectice/bloc/counter/counter_bloc.dart';
+import 'package:bloc_prectice/ui/counter/counter_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+      title: 'Bloc Prectice',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CounterScreen(),
+    ),
+    );
+  }
+}
